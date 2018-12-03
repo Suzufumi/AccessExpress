ea72435b2fd0b4d0afc9b2f2d9f605e755e60879
FumihikoSuzuki <suzufumi3722@gmail.com>
2018-12-03 14:16:28 +0900

    電波塔の読み込みとステージの建物を減らした

--- Main/GameSources/LinkObject.cpp
+++ Main/GameSources/LinkObject.cpp
@@ -13,16 +13,28 @@ namespace basecross
 		auto ptrTrans = GetComponent<Transform>();
 		auto col = AddComponent<CollisionObb>();
 		col->SetAfterCollision(AfterCollision::None);
-		col->SetDrawActive(true);
+		//col->SetDrawActive(true);
 		Quat Qt;
 		Qt.rotationRollPitchYawFromVector(Vec3(0, 0, 0));
 		ptrTrans->SetWorldPosition(m_position);
 		ptrTrans->SetQuaternion(Qt);
 		ptrTrans->SetScale(m_scale);
+
+		Mat4x4 spanMat; // モデルとトランスフォームの間の差分行列
+		spanMat.affineTransformation(
+			Vec3(1.0f, 1.0f, 1.0f),
+			Vec3(0.0f, 0.0f, 0.0f),
+			Vec3(0.0f, 0.0f, 0.0f),
+			Vec3(0.0f, -0.5f, 0.0f)
+		);
+
 		//描画コンポーネントの追加
 		auto drawComp = AddComponent<PNTStaticDraw>();
 		//描画コンポーネントに形状（メッシュ）を設定
+		//drawComp->SetMeshResource(L"TOWER_MODEL");
+		//drawComp->SetTextureResource(L"TOWER_TX");
 		drawComp->SetMeshResource(L"DEFAULT_CUBE");
+		drawComp->SetMeshToTransformMatrix(spanMat);
 		drawComp->SetDiffuse(Col4(0.0f, 1.0f, 0.0f, 1.0f));
 		GetStage()->GetSharedObjectGroup(L"Link")->IntoGroup(GetThis<GameObject>());
 	}
