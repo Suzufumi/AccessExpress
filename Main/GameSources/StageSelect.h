508314bf8b540110bdb9088a761c476694eef75d
FumihikoSuzuki <suzufumi3722@gmail.com>
2018-12-10 08:12:18 +0900

    ステセレに番号を追加、急ぎでコピペしてきたため変数の名前がぐちゃぐちゃ

--- Main/GameSources/StageSelect.h
+++ Main/GameSources/StageSelect.h
@@ -1,13 +1,23 @@
 #pragma once
 #include "stdafx.h"
-
 namespace basecross {
+class AnimSprite;
+
 	class StageSelect : public Stage {
 		//ビューの作成
 		void CreateViewLight();
+		void CreateStageNum();
+		//スプライトのweak_ptrの配列
+		vector<weak_ptr<AnimSprite>> m_SpVec;
+		bool m_CntrolLock;
 	public:
+		StageSelect()
+			: Stage(), m_CntrolLock(false), m_SpVec(5)
+		{}
 		virtual ~StageSelect() {};
 		virtual void OnCreate()override;
 		virtual void OnUpdate()override;
+		void StageSel();
+		void ChangeSelect(int num);
 	};
 }
