4c023e0c65bf15fc809ad9d1077e70331f41f15e
WatanabeKakeru <0609runa@gmail.com>
2019-05-28 10:52:35 +0900

    近づくとプレイヤーの速度が上がるオブジェクトの削除

--- Main/GameSources/FixedGameObject.cpp
+++ Main/GameSources/FixedGameObject.cpp
@@ -134,38 +134,6 @@ namespace basecross {
 		drawComp->SetDiffuse(col);
 	}
 
-	//---------------------------------------------------------------------------------------------
-	//電波塔のコンストラクタ
-	//-------------------------------------------------------------------------------------------
-	RadioTower::RadioTower(const shared_ptr<Stage>& stagePtr, Vec3 pos, Vec3 scale,float acceleration) 
-		:GameObject(stagePtr),m_position(pos),m_scale(scale),m_acceleration(acceleration)
-	{
-	}
-	void RadioTower::OnCreate() {
-		auto ptrTrans = GetComponent<Transform>();
-		auto col = AddComponent<CollisionObb>();
-		col->SetAfterCollision(AfterCollision::None);
-		Quat Qt;
-		Qt.rotationRollPitchYawFromVector(Vec3(0, 0, 0));
-		ptrTrans->SetWorldPosition(m_position);
-		ptrTrans->SetQuaternion(Qt);
-		ptrTrans->SetScale(m_scale);
-		//描画コンポーネントの追加
-		auto drawComp = AddComponent<PNTStaticDraw>();
-		//描画コンポーネントに形状（メッシュ）を設定
-		drawComp->SetMeshResource(L"DEFAULT_TETRAHEDRON");
-		//SetAlphaActive(true);
-		//drawComp->SetFogEnabled(true);
-		//自分に影が映りこむようにする
-		Col4 Color(1.0f, 1.0f, 1.0f, 0.7f);
-		drawComp->SetDiffuse(Color);
-	}
-	void RadioTower::OnUpdate() {
-
-	}
-	float RadioTower::GetAcceleration() {
-		return m_acceleration;
-	}
 
 	//--------------------------------------------------------------------------------------------------------------
 	// 背景用のスプライトを作成
